2d222542ae6f7c25911a2c6ea5f6196f3a6ede2c
onegyeol <chlgksruf3414@gmail.com>
2024-05-23 12:37:51 +0900

    jpa 실전 예제 1

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); //애플리케이션 전체에 한개만 생성됨
+        EntityManager em = emf.createEntityManager(); //고객 요청 올때마다 사용하고 버리기 반복
+
+        EntityTransaction tx = em.getTransaction(); //jpa의 모든 데이터 변경은 트랜잭션 안에서 진행필요
+        tx.begin(); //db 트랜잭션 시작
+
+        try{
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        emf.close();
+    }
+}

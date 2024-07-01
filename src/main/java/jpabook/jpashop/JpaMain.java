5bf817d5c960748068a385a8c0297bd5c800cc43
onegyeol <chlgksruf3414@gmail.com>
2024-07-01 12:48:37 +0900

    jpa 실전 예제 1

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,6 +16,10 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction(); //jpa의 모든 데이터 변경은 트랜잭션 안에서 진행필요
         tx.begin(); //db 트랜잭션 시작
         try{
+
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+
 
             tx.commit();
         }catch(Exception e){
